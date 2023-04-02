455e25d53e68a2c6ca7f42c904a41667a186536e
REDCROSS16 <belkill@mail.ru>
2023-04-03 00:09:32 +0300

    js and css update

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\LoginFormType;
+use App\Form\RegisterFormType;
+use Symfony\Component\HttpFoundation\Request
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $user = new User();
+        // login form
+        $loginForm = $this->createForm(LoginFormType::class, $user);
+
+        // register form
+        $registerForm = $this->createForm(RegisterFormType::class, $user);
+
         return $this->render('login/main.html.twig');
     }
 }
