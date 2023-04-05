a0a3be742d3afe287377f7c2ae78119f084ebae2
REDCROSS16 <belkill@mail.ru>
2023-04-05 23:55:03 +0300

    js and css update

--- src/Controller/RegisterAction.php
+++ src/Controller/RegisterAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,5 +13,8 @@ class RegisterAction extends AbstractController
     public function __invoke(Request $request)
     {
         $data = $request->request->all();
+        dd($request);
+        $user = new User();
+        $user->setName();
     }
 }
