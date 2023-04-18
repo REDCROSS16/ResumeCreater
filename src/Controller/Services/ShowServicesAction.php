228e794a2d07a95a772c16449c35df61c537ad13
REDCROSS16 <belkill@mail.ru>
2023-04-18 23:58:51 +0300

    js and css update

---
+++ src/Controller/Services/ShowServicesAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Services;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ShowServicesAction extends AbstractController
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
